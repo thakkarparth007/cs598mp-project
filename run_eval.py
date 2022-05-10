436b03c3d32f80cd4e5eb6bb09d3704d6598978e
Parth <thakkarparth007@gmail.com>
2022-05-10 04:56:49 +0000

    Add evaluation logs for distlock
    Signed-off-by: Parth <thakkarparth007@gmail.com>

--- run_eval.py
+++ run_eval.py
@@ -9,13 +9,13 @@ ID3 = ['--no-id3', '--id3']
 ITER_DEEP = ['--no-iter-deep', '--iter-deep']
 PROTOCOL = ['dist_lock'] # ricart_aggarwal not in this list because it's on a different branch currently.
 
-TIME_LIMIT = 30
+TIME_LIMIT = 90
 NUM_ITERS  = 10000
 
 variants = list(product(CHEAP, ID3, ITER_DEEP, PROTOCOL))
 
 for i, (cheap, id3, iter_deep, protocol) in enumerate(tqdm(variants)):
-    run_name = f'eval_{i}_{cheap}_{id3}_{iter_deep}_{protocol}'
+    run_name = f'eval_{i}_{cheap}_{id3}_{iter_deep}_{protocol}_90min'
     opts = f'--time-limit {TIME_LIMIT} --num-iters {NUM_ITERS}'
 
     cmd = f'./run.sh --run-name {run_name} {opts} {cheap} {id3} {iter_deep} --protocol {protocol} > ./eval_logs/{run_name}.log'
