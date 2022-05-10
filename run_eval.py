d33d2d7399a974872053ab9ad556bf440133ac55
Parth <thakkarparth007@gmail.com>
2022-05-10 06:39:23 +0000

    Add results for ricart agrawala
    Signed-off-by: Parth <thakkarparth007@gmail.com>

--- run_eval.py
+++ run_eval.py
@@ -6,16 +6,16 @@ from itertools import product
 
 CHEAP = ['--no-cheap', '--cheap']
 ID3 = ['--no-id3', '--id3']
-ITER_DEEP = ['--no-iter-deep', '--iter-deep']
-PROTOCOL = ['dist_lock'] # ricart_aggarwal not in this list because it's on a different branch currently.
+ITER_DEEP = ['--iter-deep'] # ['--no-iter-deep', '--iter-deep']
+PROTOCOL = ['ricart_agrawala'] # ['dist_lock'] # ricart_aggarwal not in this list because it's on a different branch currently.
 
-TIME_LIMIT = 90
+TIME_LIMIT = 30
 NUM_ITERS  = 10000
 
 variants = list(product(CHEAP, ID3, ITER_DEEP, PROTOCOL))
 
 for i, (cheap, id3, iter_deep, protocol) in enumerate(tqdm(variants)):
-    run_name = f'eval_{i}_{cheap}_{id3}_{iter_deep}_{protocol}_90min'
+    run_name = f'eval_{i}_{cheap}_{id3}_{iter_deep}_{protocol}'
     opts = f'--time-limit {TIME_LIMIT} --num-iters {NUM_ITERS}'
 
     cmd = f'./run.sh --run-name {run_name} {opts} {cheap} {id3} {iter_deep} --protocol {protocol} > ./eval_logs/{run_name}.log'
